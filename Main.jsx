48b7d4c52ba0e0be40d2b48d80316445f3122c3e
DASTURCHI BOLA <a05783322@gmail.com>
2026-03-24 16:33:45 +0500

    Add files via upload

---
+++ Main.jsx
@@ -0,0 +1,21 @@
+import SectionOne from './SectionOne/SectionOne';
+import SectionTwo from './SectionTwo/SectionTwo';
+import SectionThree from './SectionThree/SectionThree';
+import SectionFour from './SectionFour/SectionFour';
+import './Main.css';
+
+
+
+
+function Main() {
+    return (
+        <>
+            <SectionOne />
+            <SectionTwo />
+            <SectionThree />
+            <SectionFour />
+        </>
+    );
+}
+
+export default Main;
---
+++ SectionFour.jsx
@@ -0,0 +1,11 @@
+
+
+function SectionFour() {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+
+export default SectionFour;
